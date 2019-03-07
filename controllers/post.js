0b94f426eef7c678d013d5dd8ca4a783425b9d78
isabelcooper <isabel@triptease.com>
2019-03-07 14:44:47 +0000

    reroute traffic to correct url in prod

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,14 @@
 const postModel = require("../models/lib/post.js")
-
+let isDeployed = process.env.PORT
 exports.post_create = function (req, res) {
-  postModel.saveToDB(req.query.content)
-  res.redirect("http://localhost:3000/feed")
-}
+  postModel.saveToDB(req.query.content);
+  if(isDeployed) {
+    res.redirect("https://whispering-stream-75013.herokuapp.com/feed")
+  }
+  else {
+    res.redirect("http://localhost:3000/feed")
+  }
+};
 
 exports.post_show = async function (req, res) {
   let data = await postModel.getPosts()
