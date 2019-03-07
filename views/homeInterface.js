735c3b3c1bc4da88bae51d901eedc26dcc1d02b8
sg1110 <saule.guzyte@tesco.com>
2019-03-07 13:34:31 +0000

    Messages displayed in div with an id of a message

--- views/homeInterface.js
+++ views/homeInterface.js
@@ -1,13 +1,16 @@
 const loadMessages = async () => {
   const response = await fetch ('/post')
   const jresponse = await response.json()
+  console.log(jresponse)
   formatMessages(jresponse)
 }
 
 const formatMessages = (jresponse) => {
   jresponse.forEach( (element) => {
-    $('#postContainer').append(element.message)
+    $('#postContainer').append(`<div id=${element.id}> ${element.message}<div>`)
   })
 }
 
+// $('#parent').append('<div>hello</div>');
+// // or
 loadMessages()
