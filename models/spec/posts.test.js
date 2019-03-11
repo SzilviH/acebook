467e4c91655489a24d489de9c52f99164fd9f963
isabelcooper <isabel@triptease.com>
2019-03-11 10:59:03 +0000

    test single test

--- models/spec/posts.test.js
+++ models/spec/posts.test.js
@@ -1,49 +1,61 @@
 const Post = require("../lib/post.js");
-// const DatabaseCleaner = require('database-cleaner');
-// dbcleaner = new DatabaseCleaner('postgresql')
 const savePost = require("../lib/post").savePost
-const connection = require("../../database/connection")
+// const connection = require("../../database/connection")
 
-// beforeEach(() => {
-//   connection.pool.query("TRUNCATE TABLE posts")
+// const Pool = require("pg").Pool;
+const { Client } = require("pg");
+const client = new Client({
+  connectionString: process.env.DATABASE_URL ,
+    ssl: !process.env.LOCAL
+});
+const dotenv = require('dotenv');
+dotenv.config();
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL ,
+//     ssl: !process.env.LOCAL
 // });
 
 describe('creates an instance of post', () => {
-  it("should have a message paramater", async () => {
-    await connection.pool.query("TRUNCATE TABLE posts")
+  it("should have a message paramater", async() => {
+    await client.connect();
+    await client.query("TRUNCATE TABLE posts");
     let post = new Post(1, "message");
+    await client.end()
     expect(post.message).toEqual("message");
+
   });
-  it("should have an id", async () => {
-    await connection.pool.query("TRUNCATE TABLE posts")
-    let post = new Post(2, "message");
-    expect(post.id).toEqual(2);
-  });
+
+  // it("should have an id", async () => {
+  //   // await pool.query("TRUNCATE TABLE posts")
+  //   let post = new Post(2, "message");
+  //   expect(post.id).toEqual(2);
+  // });
 });
 
-describe('saving posts',  () => {
-  it("saves post to databse",  async () => {
-    await connection.pool.query("TRUNCATE TABLE posts")
-    await Post.saveToDB("hello")
-    let result = await connection.pool.query("SELECT * FROM posts ORDER BY id DESC LIMIT 1")
-    expect(result.rows[0].message).toEqual('hello')
-  });
-  });
+// describe('saving posts',  () => {
+//   it("saves post to databse",  async () => {
+//     await pool.query("TRUNCATE TABLE posts")
+//     await Post.saveToDB("hello")
+//     let result = await pool.query("SELECT * FROM posts ORDER BY id DESC LIMIT 1")
+//     expect(result.rows[0].message).toEqual('hello')
+//   });
+//   });
 
-  describe('getting posts',  () => {
-    it("gets all posts from db",  async () => {
-      await connection.pool.query("TRUNCATE TABLE posts")
-      await Post.saveToDB("hello")
-      await Post.saveToDB("second post")
-      let result = await Post.getPosts()
-      expect(result.length).toEqual(2);
-    });
-    it("returns an array of post objects", async () =>{
-      await connection.pool.query("TRUNCATE TABLE posts")
-      await Post.saveToDB("hello")
-      await Post.saveToDB("second post")
-      let result = await Post.getPosts()
-      expect(result[0] instanceof Post).toEqual(true); 
-    })
-
-  });
+  // describe('getting posts',  () => {
+  //   it("gets all posts from db",  async () => {
+  //     await pool.query("TRUNCATE TABLE posts")
+  //     await Post.saveToDB("hello")
+  //     await Post.saveToDB("second post")
+  //     let result = await Post.getPosts()
+  //     expect(result.length).toEqual(2);
+  //   });
+  //   it("returns an array of post objects", async () =>{
+  //     await pool.query("TRUNCATE TABLE posts")
+  //     await Post.saveToDB("hello")
+  //     await Post.saveToDB("second post")
+  //     let result = await Post.getPosts()
+  //     expect(result[0] instanceof Post).toEqual(true);
+  //   })
+  //
+  // });
