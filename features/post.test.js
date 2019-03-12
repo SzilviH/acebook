e8f390a6b2307cff25bf5d7afb03383efb43e201
Tom Damant <tom@gluon.local>
2019-03-12 11:17:59 +0000

    more posting feaure tests

--- features/post.test.js
+++ features/post.test.js
@@ -4,9 +4,10 @@ describe('adding a post', () => {
     await page.goto('http://localhost:5000/feed');
   });
 
-  it("asks users for a post", async () => {
-    await expect(page).toMatch("What's on your mind?");
+  it("asks users for a post", () => {
+     expect(page).toMatch("What's on your mind?");
   });
+
   it('users can add posts ', async () => {
     await expect(page).toFillForm('form[name="addPost"]', {
       content: 'My first post'
@@ -16,11 +17,28 @@ describe('adding a post', () => {
     await expect(page).toMatch('My first post')
   });
 
-  describe('posts have dates', () => {
-    it("displays the time it was created", async () => {
-      let now = await Date(Date.now()).toString().substring(1, 15);
-      await expect(page).toMatch(now);
-    });
+  it("can handle special characters", async () => {
+    await page.type("#postContent", "punctuation's $?%@!\"");
+    await page.click('#submit');
+    await expect(page).toMatch("punctuation's $?%@!\"")
   });
 
+  it("can handle new lines", async () => {
+      await page.type("#postContent", "first line");
+      await page.screenshot({path: 'first-line.png'});
+      await page.keyboard.press('Enter');
+      await page.screenshot({path: 'second-line.png'});
+      await page.type("#postContent", "second line");
+      await page.screenshot({path: 'third-line.png'});
+      await page.click('#submit');
+      await page.screenshot({path: 'fourth-line.png'});
+      await expect(page).toMatch("first line\nsecond line")
+  });
+
+
+  it("displays the time it was created", async () => {
+    let now = await Date(Date.now()).toString().substring(1, 15);
+    await expect(page).toMatch(now);
+  });
 });
+//
