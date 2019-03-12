f20461533828f88b0c9859adbc7cfd3a0eec276b
sg1110 <saule.guzyte@tesco.com>
2019-03-12 15:02:23 +0000

    post test fixed

---
+++ features/login.test.js
@@ -0,0 +1,39 @@
+signin = require("../views/loginInterface")
+
+puppeteer = require('puppeteer')
+describe('Incognito mode:', () => {
+
+  beforeAll(async () => {
+    const browser = await puppeteer.launch()
+    const context = await browser.createIncognitoBrowserContext()
+    const page = await context.newPage()
+  });
+
+  it("should be able to access home page", async () => {
+    await page.goto('http://localhost:5000');
+    await expect(page).toMatch("Please log in to access Acebook")
+  });
+
+  it("should not be able to access feed page without logging in", async () => {
+    // await page.screenshot({path: 'b.png'})
+    await page.goto('http://localhost:5000/feed');
+    await page.waitForNavigation({'waitUntil' : 'networkidle0'})
+    //await page.screenshot({path: 'b2.png'})
+    const url = await page.evaluate(() => location.href);
+    await expect(url).toBe('http://localhost:5000/')
+    await expect(page).toMatch("Please log in to access Acebook")
+  });
+
+  // it('should have a google login button', async() => {
+  //   await page.goto('http://localhost:5000');
+  //   await expect(page).toContain("#googleLogIn")
+  // });
+
+  // it("should redirect to feed on successful log in", async () => {
+  //   await page.goto('http://localhost:5000');
+  //   await page.click('#googleLogIn');
+  //   await signin.onSignIn()
+  //   await page.screenshot({path: 'b2.png'})
+  // })
+
+});
