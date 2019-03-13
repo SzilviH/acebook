8a0309ba27976f5188f35a733e81f2ad8c6157ec
isabelcooper <isabel@triptease.com>
2019-03-13 12:06:31 +0000

    refactor

--- views/feedInterface.js
+++ views/feedInterface.js
@@ -30,49 +30,40 @@ const getUser = async (content) => {
 
 
 const loadMessages = async () => {
-    const formatUserInput = (element) => {
-        return `${element.message.replace(/([\\])/g,"'").replace(/,/g,"<br/>")}`
-    };
-
-    const makeLikeButton = (element) => {
-        return `<button id=like-button-${element.id} type="button"></button>`;
-    };
-
-    const makePostDiv = (element) => {
-        return `<div id=${element.id}>`+formatUserInput(element)+`-- ${element.user} -- ${element.date}`+makeLikeButton(element)+ makeCommentBox(element) + `<div>`
-    };
-
-    const makeCommentBox = (element) => {
-        return `<form name="addComment">
-       <textarea id="commentContent" type="text" name="comment" placeholder="comment" rows ='1' cols = '60'></textarea>
-     <input id="comment-${element.id}" type="submit" name="comment" value="comment">
-     </form>`
-    }
-
-    const addLikeEventListener = (element) => {
-        let id = `like-button-${element.id}`;
-        $(`#${id}`).click(() => {
-            console.log("console hello");
-            fetch('/likes')
-        })
-    };
-
-
-    const addCommentEventListener = (element) => {
-
-        let id = `comment-${element.id}`;
-        $(`#${id}`).click(() => {
-            console.log("console hello");
-//             fetch('/likes')
-        })
-    };
-
     const formatMessages = (jresponse) => {
+        const makePostDiv = (element) => {
+            const makeLikeButton = (element) => {
+                return `<button id=like-${element.id} type="button"></button>`;
+            };
+
+            const makeCommentBox = (element) => {
+                return `<form name="addComment">
+                <textarea id="commentContent" type="text" name="comment" placeholder="comment" rows ='1' cols = '60'></textarea>
+                <input id="comment-${element.id}" type="submit" name="comment" value="comment">
+                </form>`
+            };
+
+            const formatUserInput = (element) => {
+                return `${element.message.replace(/([\\])/g,"'").replace(/,/g,"<br/>")}`
+            };
+
+            return `<div id=${element.id}>`+formatUserInput(element)+`-- ${element.user} -- ${element.date}`+makeLikeButton(element)+ makeCommentBox(element) + `<div>`
+        };
+
+        const addEventListener = (element, action) => {
+            let id = `${action}-${element.id}`;
+            $(`#${id}`).click((event) => {
+                event.preventDefault();
+                console.log("console hello");
+                fetch('/likes')
+            })
+        };
+
         $('#postContainer').empty();
         jresponse.forEach((element) => {
             $('#postContainer').append(makePostDiv(element));
-            addLikeEventListener(element);
-            addCommentEventListener(element);
+            addEventListener(element, "like");
+            addEventListener(element, "comment");
         })
     };
 
