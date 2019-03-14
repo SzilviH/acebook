06e5565929798ec9dac261b0d6b36a756f04f495
Tom Damant <tom@gluon.local>
2019-03-14 15:52:19 +0000

    posts appear with profile picture of user

--- models/lib/post.js
+++ models/lib/post.js
@@ -1,23 +1,24 @@
 const connection = require("../../database/connection");
 
 class Post {
-  constructor(id, message, user, date) {
-    this.id = id
-    this.message = message
-    this.user = user
+  constructor(id, message, user, user_image ,date) {
+    this.id = id;
+    this.message = message;
+    this.user = user;
+    this.user_image = user_image;
     this.date = date
   };
 
-  static async saveToDB(content, user) {
-   await connection.pool.query(`INSERT INTO posts (message, username) VALUES ('${content}', '${user}')`);
+  static async saveToDB(content, user, user_image) {
+      await connection.pool.query(`INSERT INTO posts (message, username, user_image) VALUES ('${content}', '${user}', '${user_image}')`);
   };
 
   static async getPosts() {
     let allPostsArray = [];
-    let allPosts = await connection.pool.query("SELECT id, message, username, date FROM posts ORDER BY date DESC");
+    let allPosts = await connection.pool.query("SELECT id, message, username, user_image, date FROM posts ORDER BY date DESC");
     allPosts.rows.forEach((element) => {
       element.date = element.date.toString().substring(0, 24);
-      allPostsArray.push(new Post(element.id, element.message, element.username, element.date))
+      allPostsArray.push(new Post(element.id, element.message, element.username, element.user_image, element.date))
     });
     return allPostsArray;
   }
--- views/feedInterface.js
+++ views/feedInterface.js
@@ -1,9 +1,11 @@
 let user = localStorage.getItem("username");
+let user_image = localStorage.getItem("image");
 $(document).ready(function() {
     if (user === null) {
         $(location).attr('href', '/')
     } else {
-        loadMessages()
+        loadMessages();
+        $("#post-form").prepend(`<img src = ${user_image}>`)
     }
     $('#submit').click(function (event) {
         event.preventDefault();
@@ -14,7 +16,7 @@ $(document).ready(function() {
 });
 const getUser = async (content) => {
     await $.ajax({
-        url: "/post/create?userid=" + user + "&content=" + encodeURIComponent(content),
+        url: "/post/create?userid=" + user + "&content=" + encodeURIComponent(content) + "&user_image="+user_image,
         success: () => {
         },
         complete: () => {
@@ -74,7 +76,7 @@ const loadMessages = async () => {
             const makeCommentPlaceholder = (element) => {
                 return `<div id="placeholder-${element.id}"></div><br> <br>`
             };
-            return `<div id=${element.id}>`+formatUserInput(element)+`-- ${element.user} -- ${element.date}`+makeLikeButton(element) + makeCommentDiv(element) + makeCommentPlaceholder(element) + makeCommentBox(element) + relevantLikes(element.id) + `<div>`
+            return `<div id=${element.id}><img src = ${element.user_image}>`+formatUserInput(element)+`-- ${element.user} -- ${element.date}`+makeLikeButton(element) + makeCommentDiv(element) + makeCommentPlaceholder(element) + makeCommentBox(element) + relevantLikes(element.id) + `<div>`
 
         };
         const addEventListener = (element, action) => {
